e4dce9187b12691de7e33c3fc1ccf3fdaefe525a
AndrewJMagee <andrew.magee.j@gmail.com>
2019-10-19 18:18:10 -0400

    removed keys

--- message.py
+++ message.py
@@ -1,8 +1,8 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
 
-account_sid = 'AC0f0d0cc655f0a7d78a5985be18f9012f'
-auth_token = '085569e09d7c5bb4675d04eae7cb8c9f'
+account_sid = 'xxx'
+auth_token = 'xxx'
 client = Client(account_sid, auth_token)
 
 
