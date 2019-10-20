f351e0e8b7ad61421d68d4e1773026d0e9897237
Jeff Chow <jeffchow@mit.edu>
2019-10-19 20:25:20 -0400

    added run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+# create/start venv
+python -m venv venv
+source venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+
+# required as a fix for an obscure fork() bug
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+# run app
+export FLASK_APP=app.py
+flask run
